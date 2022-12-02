1d6449f876ac7962004cd3be9aa76d58953afe46
Jingren Zhou <jingrenzhou302@gmail.com>
2022-12-02 09:34:51 +0800

    add auto close cmds for nivmtree
    Signed-off-by: Jingren Zhou <jingrenzhou302@gmail.com>

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -10,6 +10,45 @@ local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
 
 -- General settings:
 --------------------
+-- Reopen last file
+vim.cmd([[
+autocmd BufRead * autocmd FileType <buffer> ++once
+\ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$")
+\ | exe 'normal! g`"' | endif
+]])
+--[[
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
+]]
+
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
+-- nvim-tree is also there in modified buffers so this function filter it out
+local modifiedBufs = function(bufs)
+    local t = 0
+    for _,v in pairs(bufs) do
+        if v.name:match("NvimTree_") == nil then
+            t = t + 1
+        end
+    end
+    return t
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and
+        vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
+        modifiedBufs(vim.fn.getbufinfo({bufmodified = 1})) == 0 then
+            vim.cmd "quit"
+        end
+    end
+})
 
 -- Highlight on yank
 augroup('YankHighlight', { clear = true })
