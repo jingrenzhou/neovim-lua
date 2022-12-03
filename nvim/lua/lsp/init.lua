55f6d42a5d53dceceb3ffeaca4b8164af291df77
Jingren Zhou <jingrenzhou302@gmail.com>
2022-12-03 12:10:49 +0800

    lsp: remove unless code
    Signed-off-by: Jingren Zhou <jingrenzhou302@gmail.com>

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,25 +1,9 @@
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver' }
-
 local lspconfig_status_ok, lspconfig = pcall(require, 'lspconfig')
 if not lspconfig_status_ok then
   return
-end
-
-local lspconfig = require("lspconfig")
--- Call setup
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    root_dir = root_dir,
-    capabilities = capabilities,
-    flags = {
-      -- default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
-  }
 end
 
 -- see : help mason-lspconfig-dynamic-server-setup to detail
