e089685ffca254f1b2d71061941104fac1194056
Jingren Zhou <jingrenzhou302@gmail.com>
2022-12-04 19:23:56 +0800

    modify nvim-tree
    bufferline: copy for lunarvim plugins:https://github.com/LunarVim/LunarVim.git
    
    Signed-off-by: Jingren Zhou <jingrenzhou302@gmail.com>

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,238 +1,209 @@
------------------------------------------------------------
--- File manager configuration file
------------------------------------------------------------
+local icons = require("utils.icons")
 
--- Plugin: nvim-tree
--- url: https://github.com/kyazdani42/nvim-tree.lua
+local config = {
+	ignore_ft_on_setup = {
+		"startify",
+		"dashboard",
+		"alpha",
+	},
+	auto_reload_on_write = false,
+	hijack_directories = {
+		enable = false,
+	},
+	update_cwd = true,
+	diagnostics = {
+		enable = true,
+		show_on_dirs = false,
+		icons = {
+			hint = icons.diagnostics.BoldHint,
+			info = icons.diagnostics.BoldInformation,
+			warning = icons.diagnostics.BoldWarning,
+			error = icons.diagnostics.BoldError,
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	git = {
+		enable = true,
+		ignore = false,
+		timeout = 200,
+	},
+	view = {
+		width = 30,
+		hide_root_folder = false,
+		side = "left",
+		mappings = {
+			custom_only = false,
+			list = {},
+		},
+		number = false,
+		relativenumber = false,
+		signcolumn = "yes",
+	},
+	renderer = {
+		indent_markers = {
+			enable = false,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				none = " ",
+			},
+		},
+		icons = {
+			webdev_colors = true,
+			show = {
+				git = true,
+				folder = true,
+				file = true,
+				folder_arrow = true,
+			},
+			glyphs = {
+				default = icons.ui.Text,
+				symlink = icons.ui.FileSymlink,
+				git = {
+					deleted = icons.git.FileDeleted,
+					ignored = icons.git.FileIgnored,
+					renamed = icons.git.FileRenamed,
+					staged = icons.git.FileStaged,
+					unmerged = icons.git.FileUnmerged,
+					unstaged = icons.git.FileUnstaged,
+					untracked = icons.git.FileUntracked,
+				},
+				folder = {
+					default = icons.ui.Folder,
+					empty = icons.ui.EmptyFolder,
+					empty_open = icons.ui.EmptyFolderOpen,
+					open = icons.ui.FolderOpen,
+					symlink = icons.ui.FolderSymlink,
+				},
+			},
+		},
+		highlight_git = true,
+		group_empty = false,
+		root_folder_modifier = ":t",
+	},
+	filters = {
+		dotfiles = false,
+		custom = { "node_modules", "\\.cache" },
+		exclude = {},
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
+	log = {
+		enable = false,
+		truncate = false,
+		types = {
+			all = false,
+			config = false,
+			copy_paste = false,
+			diagnostics = false,
+			git = false,
+			profile = false,
+		},
+	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = false,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
+	},
+}
 
--- Keybindings are defined in `core/keymaps.lua`:
--- https://github.com/kyazdani42/nvim-tree.lua#keybindings
-local status_ok, nvim_tree = pcall(require, 'nvim-tree')
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+	return
 end
 
--- Call setup.
--- See: `:help nvim-tree` 4. SETUP
--- Each of these are documented in `:help nvim-tree.OPTION_NAME`
--- nested options are documented by accessing them with `.` (eg: `:help nvim-tree.view.mappings.list`)
-nvim_tree.setup {
-  auto_reload_on_write = true,
-  create_in_closed_folder = false,
-  --disable_netrw = false, -> already disabled on `/core/options.lua`
-  hijack_cursor = false,
-  hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = true,
-  open_on_setup_file = false, -- open nvim-tree when you open any file
-  sort_by = "name",
-  root_dirs = {},
-  prefer_startup_root = false,
-  sync_root_with_cwd = false,
-  reload_on_bufenter = false,
-  respect_buf_cwd = false,
-  on_attach = "disable",
-  remove_keymaps = false,
-  select_prompts = false,
-  view = {
-    adaptive_size = false,
-    centralize_selection = false,
-    width = 32,
-    hide_root_folder = false,
-    side = "left",
-    preserve_window_proportions = false,
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
-    mappings = {
-      custom_only = false,
-      list = {
-        -- user mappings go here
-      },
-    },
-    float = {
-      enable = false,
-      quit_on_focus_loss = true,
-      open_win_config = {
-        relative = "editor",
-        border = "rounded",
-        width = 30,
-        height = 30,
-        row = 1,
-        col = 1,
-      },
-    },
-  },
-  renderer = {
-    add_trailing = false,
-    group_empty = false,
-    highlight_git = false,
-    full_name = false,
-    highlight_opened_files = "none",
-    root_folder_label = ":~:s?$?/..?",
-    indent_width = 2,
-    indent_markers = {
-      enable = false,
-      inline_arrows = true,
-      icons = {
-        corner = "└",
-        edge = "│",
-        item = "│",
-        bottom = "─",
-        none = " ",
-      },
-    },
-    icons = {
-      webdev_colors = true,
-      git_placement = "before",
-      padding = " ",
-      symlink_arrow = " ➛ ",
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-      glyphs = {
-        default = "",
-        symlink = "",
-        bookmark = "",
-        folder = {
-          arrow_closed = "",
-          arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
-    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
-    symlink_destination = true,
-  },
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  update_focused_file = {
-    enable = true,
-    update_root = true,
-    ignore_list = {},
-  },
-  ignore_ft_on_setup = {},
-  system_open = {
-    cmd = "",
-    args = {},
-  },
-  diagnostics = {
-    enable = false,
-    show_on_dirs = false,
-    debounce_delay = 50,
-    severity = {
-      min = vim.diagnostic.severity.HINT,
-      max = vim.diagnostic.severity.ERROR
-    },
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-    exclude = {},
-  },
-  filesystem_watchers = {
-    enable = true,
-    debounce_delay = 50,
-    ignore_dirs = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    show_on_dirs = true,
-    timeout = 400,
-  },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-      restrict_above_cwd = false,
-    },
-    expand_all = {
-      max_folder_discovery = 300,
-      exclude = {},
-    },
-    file_popup = {
-      open_win_config = {
-        col = 1,
-        row = 1,
-        relative = "cursor",
-        border = "shadow",
-        style = "minimal",
-      },
-    },
-    open_file = {
-      quit_on_open = false,
-      resize_window = true,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
-      },
-    },
-    remove_file = {
-      close_window = true,
-    },
-  },
-  trash = {
-    cmd = "gio trash",
-    require_confirm = true,
-  },
-  live_filter = {
-    prefix = "[FILTER]: ",
-    always_show_folders = true,
-  },
-  tab = {
-    sync = {
-      open = false,
-      close = false,
-      ignore = {},
-    },
-  },
-  notify = {
-    threshold = vim.log.levels.INFO,
-  },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      dev = false,
-      diagnostics = false,
-      git = false,
-      profile = false,
-      watcher = false,
-    },
-  },
-} -- END_OPTS
+-- Implicitly update nvim-tree when project module is active
+config.respect_buf_cwd = true
+config.update_cwd = true
+config.update_focused_file = { enable = true, update_cwd = true }
+
+local function start_telescope(telescope_mode)
+	local node = require("nvim-tree.lib").get_node_at_cursor()
+	if not node then
+		return
+	end
+	local abspath = node.link_to or node.absolute_path
+	local is_folder = node.open ~= nil
+	local basedir = is_folder and abspath or vim.fn.fnamemodify(abspath, ":h")
+	require("telescope.builtin")[telescope_mode] {
+		cwd = basedir,
+	}
+end
+
+-- Add useful keymaps
+config.view.mappings.list = {
+	{ key = {"<CR>", "o" }, action = "edit", mode = "n" },
+	{ key = "h", action = "close_node" },
+	{ key = "v", action = "vsplit" },
+	{ key = "sv", action = "split" },
+	{ key = "C", action = "cd" },
+	{ key = "gtf", action = "telescope_find_files", action_cb = start_telescope("find_files") },
+	{ key = "gtg", action = "telescope_live_grep", action_cb = start_telescope("live_grep") },
+}
+
+nvim_tree.setup(config)
+
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap("n", "tf", "<cmd>NvimTreeToggle<CR>", opts)
+
+--[[
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
+]]
+
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
+-- nvim-tree is also there in modified buffers so this function filter it out
+local modifiedBufs = function(bufs)
+    local t = 0
+    for _,v in pairs(bufs) do
+        if v.name:match("NvimTree_") == nil then
+            t = t + 1
+        end
+    end
+    return t
+end
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and
+        vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil and
+        modifiedBufs(vim.fn.getbufinfo({bufmodified = 1})) == 0 then
+            vim.cmd "quit"
+        end
+    end
+})
+
